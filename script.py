bec6eb587639a603feb40dc6bc6c7b0967d9d402
sthix <thanatosclothing@gmail.com>
2026-02-04 00:01:44 +0100

    Added new function + logic

--- script.py
+++ script.py
@@ -1,13 +1,40 @@
 import random
 
 colors: list = ['red', 'blue', 'yellow', 'green', 'purple', 'brown', 'black', 'white', 'orange']
-
+correct_letters: set = set()
+answered: bool = False
 
 def select_random_color():
     """Select a random color from the list 'colors' and return it."""
-    random_color = colors[random.randint(0, len(colors)) - 1]
+    random_color: str = random.choice(colors)
     return random_color
 
+def save_color(word):
+    [correct_letters.add(letter) for letter in word]
+    print(correct_letters)
+
+def guess_word(word):
+    guess: str = str(input("Guess a letter from a color name!\n"))
+    print(correct_letters)
+
+    if guess in word:
+        print(guess)
+    else:
+        print("– ")
+
+
+def show_guess(word, guess):
+    # Show the word with the currently correctly guessed letters
+    display_list = []
+    for letter in word:
+        if letter in word:
+            print(letter)
+        else:
+            print("_")
+
+
+    print(*display_list)
 
 if __name__ == "__main__":
-    select_random_color()
+    word = select_random_color()
+    save_color(word)
